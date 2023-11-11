80a69e2124bb6989b36769a413039d3cbfedb51d
Callum Canavan <callumcanavan@gmail.com>
2023-11-11 08:22:22 +0000

    Attention by rank plots

--- mech_int_othelloGPT/intervene.py
+++ mech_int_othelloGPT/intervene.py
@@ -9,6 +9,7 @@ from functools import partial
 import pickle
 import torch
 from tqdm import tqdm
+from icecream import ic
 
 from .data import get_othello
 from .othello import permit, OthelloBoardState, permit_reverse
@@ -145,6 +146,187 @@ def intervene(probe_dir, layers, hook, device="mps"):
         (patched_logits, modified_cache) = run_with_cache_and_hooks(
             othello_gpt,
             list((f"blocks.{layer}.hook_{hook}", hook_fns[layer]) for layer in layers),
+            partial_game,
+        )
+
+        orig_topk_preds = orig_logits[0, -1].topk(k=60)
+        modified_topk_preds = patched_logits[0, -1].topk(k=len(post_intv_valids))
+
+        orig_preds = [
+            to_board_label(ITOS[x.item()]).lower()
+            for x in orig_topk_preds.indices[: len(post_intv_valids)]
+        ]
+        modified_preds = [
+            to_board_label(ITOS[x.item()]).lower()
+            for x in modified_topk_preds.indices[: len(post_intv_valids)]
+        ]
+
+        modified_preds_cutoff = modified_preds[: len(post_intv_valids)]
+        orig_preds_cutoff = orig_preds[: len(post_intv_valids)]
+        _new_valid_gold = sorted(
+            [x for x in post_intv_valids if x not in pre_intv_valids]
+        )
+        _new_valid_pred = sorted(
+            [
+                x
+                for x in modified_preds_cutoff
+                if x in post_intv_valids and x not in pre_intv_valids
+            ]
+        )
+
+        _false_pos = [
+            x for x in modified_preds_cutoff if x not in post_intv_valids
+        ]
+        _false_negs = [
+            x for x in post_intv_valids if x not in modified_preds_cutoff
+        ]
+        false_positives.append(len(_false_pos))
+        false_negatives.append(len(_false_negs))
+
+        _false_pos_orig = [
+            x for x in orig_preds_cutoff if x not in post_intv_valids
+        ]
+        _false_negs_orig = [
+            x for x in post_intv_valids if x not in orig_preds_cutoff
+        ]
+        false_positives_null_intv.append(len(_false_pos_orig))
+        false_negatives_null_intv.append(len(_false_negs_orig))
+
+
+    errs = [
+        false_positives[idx] + false_negatives[idx]
+        for idx in range(len(false_positives))
+    ]
+    null_intv_errs = [
+        false_positives_null_intv[idx] + false_negatives_null_intv[idx]
+        for idx in range(len(false_positives_null_intv))
+    ]
+    # print("errs:", np.mean(errs))
+    # print("null intv errs:", np.mean(null_intv_errs))
+    return np.mean(errs), np.mean(null_intv_errs)
+
+
+def intervene_layer_hooks(pre_probe_dir, post_probe_dir, layer_hooks, device="mps"):
+    # print(f"Pre probe dir: {pre_probe_dir}")
+    # print(f"Post probe dir: {post_probe_dir}.")
+    # print(f"Layer hooks: {layer_hooks}")
+
+    pre_probes = {}
+    for layer in range(8):
+        pre_probes[layer] = torch.load(
+            os.path.join(pre_probe_dir, f"resid_{layer}_linear.pth"), map_location=torch.device(device)
+        )
+        pre_probes[layer].requires_grad = False
+    
+    post_probes = {}
+    for layer in range(8):
+        post_probes[layer] = torch.load(
+            os.path.join(post_probe_dir, f"resid_{layer}_linear.pth"), map_location=torch.device(device)
+        )
+        post_probes[layer].requires_grad = False
+    
+
+    def patch_heads(
+        modified_heads,
+        hook,
+        probe_layer,
+        from_player,
+        row,
+        col,
+        move_idx,
+    ):
+        add_vector = None
+        if "resid_mid" in hook.name:
+            probe = pre_probes[probe_layer]
+        elif "resid_post" in hook.name:
+            probe = post_probes[probe_layer]
+        else:
+            raise RuntimeError("Unexpected hook name.")
+        
+        if move_idx % 2 == 0:
+            # White's turn to play, flipping white's cell.
+            if from_player == 0:
+                add_vector = probe[0, ..., row, col, 2]
+            elif from_player == 2:
+                add_vector = probe[0, ..., row, col, 1]
+            else:
+                raise RuntimeError("Unexpected from_player value.")
+
+        else:
+            if from_player == 0:
+                add_vector = probe[0, ..., row, col, 1]
+            elif from_player == 2:
+                add_vector = probe[0, ..., row, col, 2]
+            else:
+                raise RuntimeError("Unexpected from_player value.")
+
+        scale = 2.3
+        modified_heads[:, -1, :] = modified_heads[:, -1, :] + (
+            scale * (add_vector / add_vector.norm())
+        )
+        return modified_heads
+
+
+    false_positives = []
+    false_negatives = []
+    false_positives_null_intv = []
+    false_negatives_null_intv = []
+    for _sample in intv_data:
+        board_state = OthelloBoardState()
+        completion = _sample["completion"]
+        board_state.update(completion)
+
+        partial_game = torch.tensor(
+            [train_dataset.stoi[s] for s in completion], dtype=torch.long
+        ).to(device)
+
+        pre_intv_valids = [
+            permit_reverse(_) for _ in board_state.get_valid_moves()
+        ]
+        pre_intv_pred = othello_gpt(partial_game[None, :])
+
+        pre_intv_pred = pre_intv_pred[0, -1, 1:]
+        padding = torch.zeros(2).to(device)
+
+        pre_intv_pred = torch.softmax(pre_intv_pred, dim=0)
+        pre_intv_pred = torch.cat(
+            [
+                pre_intv_pred[:27],
+                padding,
+                pre_intv_pred[27:33],
+                padding,
+                pre_intv_pred[33:],
+            ],
+            dim=0,
+        )
+
+        move = permit(_sample["intervention_position"])
+        r, c = move // 8, move % 8
+        board_state.state[r, c] = _sample["intervention_to"] - 1
+
+        post_intv_valids = [
+            permit_reverse(_) for _ in board_state.get_valid_moves()
+        ]
+        orig_logits, blank_cache = othello_gpt.run_with_cache(
+            partial_game, return_type="logits"
+        )
+
+        hook_fns = []
+        for _layer in range(8):
+            hook_fns.append(
+                partial(
+                    patch_heads,
+                    probe_layer=_layer,
+                    from_player=_sample["intervention_from"],
+                    row=r,
+                    col=c,
+                    move_idx=len(_sample["completion"]),
+                )
+            )
+
+        (patched_logits, modified_cache) = run_with_cache_and_hooks(
+            othello_gpt,
+            list((f"blocks.{layer}.hook_{hook}", hook_fns[layer]) for layer, hook in layer_hooks),
             partial_game,
         )
 
